957376a00080c8b4bbb131bc1ef8447e986f8575
muhammad ismail <muhismailmks@gmail.com>
2025-01-28 21:00:29 +0800

    uppdate

--- test/test.js
+++ test/test.js
@@ -1,26 +1,120 @@
-var items = document.querySelectorAll(".list__item")
+document.addEventListener("DOMContentLoaded", () => {
+ 
 
-items.forEach(item => {
-  var itemTitle = item.querySelector(".list__item__title")
-  var itemTitleOutline = item.querySelector(".list__item__titleOutline")
-  
-  var itemImg = item.querySelector(".list__item img")
-  
-  var itemTL = gsap.timeline({scrollTrigger: {
-    trigger: item,
-    start: "0% 75%",
-    end: "25% 50%",
-    scrub: 3,
-  }})
-  
-  itemTL.fromTo(itemTitle, {scale: 2, y: "100%"}, {scale: 1, y: "0%", ease: "power2.inOut"}, 0)
-  itemTL.fromTo(itemTitleOutline, {scale: 2, y: "100%"}, {scale: 1, y: "0%", ease: "power2.inOut"}, 0)
-  
-  gsap.fromTo(itemImg, {x: "60vw", y : "60vh", rotate: -30}, {x: "-60vw", y: "-60vh", rotate: 30, ease: "none", scrollTrigger: {
-    trigger: item,
-    start: "50% 100%",
-    end: "100% 50%",
-    scrub: 3,
-  }})
 
-})
+
+
+  //////////////////////////////////////
+  // hero
+  // const triggerElement = window.innerWidth < 1024 ? '.about' : '.hero_section';
+  // const xElementHero = window.innerWidth < 1024 ? '20vw' : '50vw';
+
+  // gsap.fromTo('.plane_animation', { x: xElementHero, }, {
+  //     x: "-60vw", ease: "none", scrollTrigger: {
+  //         trigger: triggerElement,
+  //         start: "50% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 5,
+  //     }
+  // });
+
+
+  // // about  
+  // gsap.fromTo('.ballon_left', { x: '-20vw', }, {
+  //     x: "30vw", ease: "none", scrollTrigger: {
+  //         trigger: '.about',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 1,
+  //     }
+  // });
+
+  // gsap.fromTo('.ballon_right', { x: '30vw', }, {
+  //     x: "-30vw", ease: "none", scrollTrigger: {
+  //         trigger: '.about',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 1,
+  //     }
+  // });
+
+  // // tokenomics
+  // gsap.fromTo('.token1', { x: '-10vw', y: '-0vw' }, {
+  //     x: "10vw", y: '10vw', ease: "none", scrollTrigger: {
+  //         trigger: '.token_image',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 3,
+  //     }
+  // });
+
+  // gsap.fromTo('.token2', { x: '20vw', y: '0vw' }, {
+  //     x: "-20vw", y: '10vw', ease: "none", scrollTrigger: {
+  //         trigger: '.token_image',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 3,
+  //     }
+  // });
+
+  // gsap.fromTo('.token3', { x: '-10vw', y: '10vw' }, {
+  //     x: "10vw", y: '0vw', ease: "none", scrollTrigger: {
+  //         trigger: '.token_image',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 4,
+  //     }
+  // });
+
+  // gsap.fromTo('.token4', { x: '20vw', y: '10vw' }, {
+  //     x: "-20vw", y: '0vw', ease: "none", scrollTrigger: {
+  //         trigger: '.token_image',
+  //         start: "0% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 4,
+  //     }
+  // });
+
+  // // roadmap
+  // // image
+  // gsap.fromTo('.image_roadmap_container', { y: '0vw', }, {
+  //     y: "20vw", ease: "none", scrollTrigger: {
+  //         trigger: '.image_roadmap',
+  //         start: "50% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 5,
+  //     }
+  // });
+
+
+  // // maps
+  // const map1 = window.innerWidth < 1024 ? '.map1' : '.image_roadmap';
+  // const map2 = window.innerWidth < 1024 ? '.map2' : '.image_roadmap';
+  // const map3 = window.innerWidth < 1024 ? '.map3' : '.image_roadmap';
+
+  // gsap.fromTo('.map1', { y: '0vw', }, {
+  //     y: "-300px", ease: "none", scrollTrigger: {
+  //         trigger: map1,
+  //         start: "50% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 3,
+  //     }
+  // });
+  // gsap.fromTo('.map2', { y: '0vw', }, {
+  //     y: "-200px", ease: "none", scrollTrigger: {
+  //         trigger: map2,
+  //         start: "50% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 3,
+  //     }
+  // });
+  // gsap.fromTo('.map3', { y: '0vw', }, {
+  //     y: "-100px", ease: "none", scrollTrigger: {
+  //         trigger: map3,
+  //         start: "50% 100%",
+  //         end: "100% 50%", 
+  //         scrub: 3,
+  //     }
+  // });
+
+});
